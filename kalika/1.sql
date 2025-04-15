0f54d61ec8046d29ca8202c8e424010159d7eec0
Beeplap <ghartibeeplap@gmail.com>
2025-04-16 04:14:47 +0545

    staged

---
+++ kalika/1.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE kalikaa;
+
+USE kalikaa;
+
+CREATE TABLE bca (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR(100) NOT NULL,
+	semester INT NOT NULL,
+	address VARCHAR(255) NOT NULL
+);
